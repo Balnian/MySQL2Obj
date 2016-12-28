9a1489960dcd478fce4913daff1aff6bfbe34d81
Francis <super_f_qc@hotmail.com>
2016-12-28 16:35:23 -0500

    Ajoutez des fichiers projet.

---
+++ src/MySQL2Obj/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MySQL2Obj
+{
+    public class Class1
+    {
+        public Class1()
+        {
+        }
+    }
+}
